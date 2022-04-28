4f7349dc9cbcec3ea0200e6812e903de7e012bdb
Trisemya <trisemya@gmail.com>
2022-04-29 02:25:05 +0300

    feat: add carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (isNaN(parseFloat(sampleActivity)) || typeof sampleActivity !== 'string' || 0 >= +sampleActivity || +sampleActivity > MODERN_ACTIVITY) {
+    return false
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD
+  let t = Math.log(MODERN_ACTIVITY / +sampleActivity)
+  t /= k
+  return Math.ceil(t)
 }
 
 module.exports = {
