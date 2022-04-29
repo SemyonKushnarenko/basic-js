00b49f6e0d77a8622ee804bae4d2c6e2187bc697
Trisemya <trisemya@gmail.com>
2022-04-30 00:08:11 +0300

    feat: vigenere done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (rev) {
+    this.rev = rev
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(word, key) {
+    if (word === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+    const stringKey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const symbols = stringKey.split('')
+
+    const wordSymbols = word.split('')
+    let keySymbols = key.split('')
+    let k = 0
+    for (let i = 0; i < wordSymbols.length; i++) {
+      let wordIdx = symbols.indexOf(wordSymbols[i].toUpperCase())
+      let keyIdx = symbols.indexOf(keySymbols[k % keySymbols.length].toUpperCase())
+      if (wordIdx > -1) {
+        k++
+        console.log(wordIdx + ' - ' + keyIdx)
+        wordSymbols[i] = symbols[((wordIdx + keyIdx) % (symbols.length))]
+      }
+    }
+
+    if (this.rev === false) {
+      return wordSymbols.reverse().join('')
+    }
+
+    return wordSymbols.join('')
+  }
+  decrypt(word, key) {
+    if (word === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+    const stringKey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const symbols = stringKey.split('')
+
+    const wordSymbols = word.split('')
+    let keySymbols = key.split('')
+    let k = 0
+    for (let i = 0; i < wordSymbols.length; i++) {
+      let wordIdx = symbols.indexOf(wordSymbols[i].toUpperCase())
+      let keyIdx = symbols.indexOf(keySymbols[k % keySymbols.length].toUpperCase())
+      if (wordIdx > -1) {
+        k++
+        console.log(wordIdx + ' - ' + keyIdx)
+        wordSymbols[i] = symbols[((wordIdx - keyIdx + 26) % (symbols.length))]
+      }
+    }
+
+    if (this.rev === false) {
+      return wordSymbols.reverse().join('')
+    }
+
+    return wordSymbols.join('')
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+
+//AEIHQX SX DLLU!
 
 module.exports = {
   VigenereCipheringMachine
